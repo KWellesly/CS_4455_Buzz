c08a75084cf2303de195e55382f8180faed98d5b
EricLeGT <31485775+EricLeGT@users.noreply.github.com>
2020-02-16 01:46:40 -0500

    Cleanup and added my own sandbox scene

---
+++ Buzz/Assets/ai_test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ai_test : MonoBehaviour
+{
+    public Animator anim;
+
+    private Transform tr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        tr.Translate(Vector3.forward * Time.deltaTime);
+    }
+}
+
