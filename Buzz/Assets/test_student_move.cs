86efea9b5a8e112d80b5e7a2e5de74a04e240298
EricLeGT <31485775+EricLeGT@users.noreply.github.com>
2020-02-16 14:44:55 -0500

    Added test student w/ root motion

---
+++ Buzz/Assets/test_student_move.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_student_move : MonoBehaviour
+{
+    private Animator anim;
+    public float MaxSpeed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (anim == null)
+        {
+            return;
+        }
+        var x = Input.GetAxis("Horizontal");
+        var y = Input.GetAxis("Vertical");
+        move(x, y);
+    }
+    void move(float x, float y)
+    {
+        anim.SetFloat("VelX", x);
+        anim.SetFloat("VelY", y);
+        //transform.position += (Vector3.forward * MaxSpeed) * y * Time.deltaTime;
+        //transform.position += (Vector3.right * MaxSpeed) * x * Time.deltaTime;
+
+    }
+}
