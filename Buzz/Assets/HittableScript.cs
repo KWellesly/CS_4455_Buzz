e96e921e35c700df97b9257eadc7b66370681ee2
chanhee-lee <clee643@gatech.edu>
2020-02-28 16:42:24 -0500

    slapping interaction

---
+++ Buzz/Assets/HittableScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator), typeof(Rigidbody))]
+public class HittableScript : MonoBehaviour
+{
+    public Animator anim;
+    public Collider player = null;
+    public bool isSlappable = false; 
+
+    private Rigidbody rbody;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.attachedRigidbody != null)
+        {
+            PowerupCollector pc = c.attachedRigidbody.gameObject.GetComponent<PowerupCollector>(); // Only SomeDude_RootMotion has ball collector 
+            if (pc != null)
+            {
+                player = c;
+            }
+            isSlappable = pc != null;
+        }
+    }
+    void OnTriggerStay(Collider c)
+    {
+        if (c.attachedRigidbody != null)
+        {
+            PowerupCollector pc = c.attachedRigidbody.gameObject.GetComponent<PowerupCollector>(); // Only SomeDude_RootMotion has ball collector 
+            if (pc != null)
+            {
+                player = c;
+            }
+            isSlappable = pc != null;
+        }
+    }
+
+    public void FixedUpdate()
+    {
+        if (player != null && (Vector3.Distance(player.transform.position, this.transform.position) > 2.2))
+        {
+            Debug.Log(Vector3.Distance(player.transform.position, this.transform.position));
+            isSlappable = false;
+        }
+
+        //anim.SetBool("isHungry", isHungry);
+    }
+}
---
+++ Buzz/Assets/SlapperScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator), typeof(Rigidbody))]
+public class SlapperScript : MonoBehaviour
+{
+    public Animator anim;
+    public Collider player = null;
+    public bool isSlappable = false;
+    public GameObject target; 
+
+    private Rigidbody rbody;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update() {
+        if (target != null && isSlappable && Input.GetButtonDown("Fire1")) {
+            anim.SetBool("slap", isSlappable);
+        }
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.attachedRigidbody != null)
+        {
+            HittableScript hs = c.attachedRigidbody.gameObject.GetComponent<HittableScript>(); // Only SomeDude_RootMotion has ball collector 
+            if (hs != null)
+            {
+                target = c.attachedRigidbody.gameObject;
+            }
+            isSlappable = hs != null;
+        }
+    }
+
+    void OnTriggerStay(Collider c)
+    {
+        if (c.attachedRigidbody != null)
+        {
+            HittableScript hs = c.attachedRigidbody.gameObject.GetComponent<HittableScript>(); // Only SomeDude_RootMotion has ball collector 
+            if (hs != null)
+            {
+                target = c.attachedRigidbody.gameObject;
+            }
+            isSlappable = hs != null;
+        }
+    }
+
+    public void FixedUpdate()
+    {
+        if (target != null && (Vector3.Distance(target.transform.position, this.transform.position) > 2.2))
+        {
+            //Debug.Log(Vector3.Distance(target.transform.position, this.transform.position));
+            isSlappable = false;
+        }
+        //anim.SetBool("isHungry", isHungry);
+    }
+}
